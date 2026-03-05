3ff8e4962410b032717bffb88b81911f09ef1700
asifdevops786-tech <asifdevops786@gmail.com>
2026-03-05 17:07:09 +0530

    Create index.js
    Add Node app

---
+++ app/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const { Pool } = require('pg');
+const app = express();
+app.use(express.json());
+
+const pool = new Pool({
+  user: 'postgres',
+  host: 'db',
+  database: 'webapp',
+  password: 'password',
+  port: 5432,
+});
+
+app.get('/', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT NOW()');
+    res.send(`Hello Docker! DB time: ${result.rows[0].now}`);
+  } catch (err) {
+    res.status(500).send('DB error: ' + err.message);
+  }
+});
+
+app.listen(3000, () => console.log('App on 3000'));
